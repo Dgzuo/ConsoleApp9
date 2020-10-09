4ac6617c9ea5db898b57bbb063c5e9ccbb2714b0
su <su@DESKTOP-7VFS5TB>
2020-10-09 18:07:42 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Numerics;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                {
+                    Console.WriteLine("Second operand is evaluated.");
+                    double a, b, c;
+                    a = int.Parse(Console.ReadLine());
+                    b = int.Parse(Console.ReadLine());
+                    c = int.Parse(Console.ReadLine());
+                    bool r = (a > 3) && (b > 3) && (c > 3);
+                    Console.WriteLine(r);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
